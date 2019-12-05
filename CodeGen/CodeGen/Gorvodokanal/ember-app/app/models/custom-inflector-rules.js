5d67812d9b80a8b00c0ae1374e5b2278865ca1af
Permyakova Ksenya <kpermyakova@ics.perm.ru>
2019-12-05 13:49:27 +0500

    Исправлено: CORS-полтика

--- CodeGen/CodeGen/Gorvodokanal/ember-app/app/models/custom-inflector-rules.js
+++ CodeGen/CodeGen/Gorvodokanal/ember-app/app/models/custom-inflector-rules.js
@@ -2,7 +2,7 @@ import Inflector from 'ember-inflector';
 
 const inflector = Inflector.inflector;
 
-inflector.irregular('address', 'Addresss');
+inflector.irregular('address', 'Addresses');
 inflector.irregular('request', 'Requests');
 inflector.irregular('task', 'Tasks');
 inflector.irregular('team', 'Teams');
